ed65b88bbb1e91ec398dd7c39f424e46d218cb00
saku-bruh <saku-bruh@proton.me>
2024-09-15 14:24:04 +0300

    hardware: Fix-ups!
    * Fix XperiaCharger SEPolicy path
    
    * Remove EQ intent-filters from XperiaAudio since we will put it on Dolby Sound/DTS:X
    
    * Fix more SEPolicy denials
    
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- XperiaModules/Charging/XperiaCharger/sepolicy/qti/SEPolicy.mk
+++ XperiaModules/Charging/XperiaCharger/sepolicy/qti/SEPolicy.mk
@@ -5,4 +5,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-BOARD_VENDOR_SEPOLICY_DIRS += hardware/sony/XperiaModules/XperiaCharger/sepolicy/qti/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/sony/XperiaModules/Charging/XperiaCharger/sepolicy/qti/vendor
